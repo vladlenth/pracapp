997bb0a7d885ebc8ed1bc3344935be1facbfbc05
Vladlen Basenko <vladbas445@gmail.com>
2025-08-06 18:06:57 +0300

    git init

---
+++ cv-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from './Components/Header';
+import CVForm from './Components/CVForm';
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <Header />
+            <CVForm />
+        </>
+    );
+}
+
+export default App;
